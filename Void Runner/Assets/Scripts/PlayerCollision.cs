340bbe0bd3412af0f493cc76f357948ece17e752
Jackson Franke <120767886+Jackson-Franke74@users.noreply.github.com>
2025-03-13 13:24:42 -0400

    checkpoint uncompleted

--- Void Runner/Assets/Scripts/PlayerCollision.cs
+++ Void Runner/Assets/Scripts/PlayerCollision.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
+    private int checkpoint;
+
+    void Start ()
+    {
+        checkpoint = 0;
+    }
+
     // Basic script to handle collisions with the player
     private void OnCollisionEnter(Collision collision)
     {
@@ -13,5 +20,24 @@ public class PlayerCollision : MonoBehaviour
             Debug.Log("Player is DEAD!");
             AudioManager.Instance.PlayDeathSFX();
         }
+
+        if (collision.collider.tag == "Checkpoint")
+        {
+            Debug.Log("Checkpoint");
+        }
+    }
+
+    private void onTriggerEnter (Collider other)
+    {
+        if (other.gameObject.tag == "Checkpoint")
+        {
+            Debug.Log("Checkpoint");
+            checkpoint++;
+        }
+    }
+
+    int getCheckpoint ()
+    {
+        return checkpoint;
     }
 }
