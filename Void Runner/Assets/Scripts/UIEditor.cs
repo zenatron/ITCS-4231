9ea46fb0bc20b68e230a80313f5aee05d09214d6
Jackson Franke <120767886+Jackson-Franke74@users.noreply.github.com>
2025-03-20 15:36:12 -0400

    text UI incomplete

---
+++ Void Runner/Assets/Scripts/UIEditor.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using TMPro;
+
+class Timer : MonoBehaviour
+{
+    public string time;
+    public TextMeshProUGUI totalTime;
+    private float secondsCount;
+    private int minuteCount;
+
+    void Start()
+    {
+        secondsCount = 0;
+        minuteCount = 0;
+        SetTimerText();
+    }
+
+
+    void Update()
+    {
+        UpdateTimerUI();
+        SetTimerText();
+    }
+
+    void SetTimerText()
+    {
+        totalTime.text = "Time: " + time.ToString();
+    }
+
+    //call this on update
+    public void UpdateTimerUI()
+    {
+        //set timer UI
+        secondsCount += Time.deltaTime;
+        time = minuteCount + " m, " + (int)secondsCount + " s ";
+        if (secondsCount >= 60)
+        {
+            minuteCount++;
+            secondsCount = 0;
+        }
+
+    }
+}
+
+/*[CreateAssetMenu(fileName = "UIEditor", menuName = "Scriptable Objects/CheckpointState")]
+class CheckpointState : ScriptableObject
+{
+    public int lastCheckpointID;
+    public Vector3 lastCheckpointPosition;
+}
+*/
